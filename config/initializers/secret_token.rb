81f5a8c82c4865bd89e4b84f513f527f37deb83a
Fabio Iodice Gasparro <fabiogasparro@gmail.com>
2011-10-07 20:11:59 -0300

    first commit of tertulia project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tertulia::Application.config.secret_token = '67bae5e87d8c28e93663df399602ac78261c2b6e097e3705dc3f4bc62221e018b4368e528a5ecfbe9f82c4f56e917621f8ee4d025100b6df9d3b0263eba66720'
